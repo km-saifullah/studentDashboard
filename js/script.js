1a44d11e2760f71e0ac2e64ca5cd71f5521cd74f
km-saifullah <kmsaifullah16@gmail.com>
2023-11-15 12:38:25 +0600

    student marks and details show in the browser

--- js/script.js
+++ js/script.js
@@ -3,10 +3,37 @@ let takeInput = (selectorName) => {
   return document.querySelector(selectorName);
 };
 
-// buttons
+// button, sections, student info
 let submitBtn = takeInput(".submitBtn");
+let studentForm = takeInput(".students_form");
+let dashboardDetils = takeInput(".dashboard_details");
+let studentName = takeInput(".studentName");
+let idNumber = takeInput(".idNumber");
+let studnetClass = takeInput(".studnetClass");
+let studentSession = takeInput(".studentSession");
+let totalMark = takeInput(".totalMark");
+let marksInfo = takeInput(".markChart");
 
+dashboardDetils.style.display = "none";
+
+// array for marks
 let marks = [];
+let totalScore = 0;
+
+// clear input fields
+let clearInputs = (inputValue) => {
+  takeInput(".firstName").value = "";
+  takeInput(".lastName").value = "";
+  takeInput(".sId").value = "";
+  takeInput(".className").value = "";
+  takeInput(".session").value = "";
+  takeInput(".bangla").value = "";
+  takeInput(".english").value = "";
+  takeInput(".math").value = "";
+  takeInput(".physics").value = "";
+  takeInput(".chemistry").value = "";
+  takeInput(".biology").value = "";
+};
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -15,17 +42,61 @@ submitBtn.addEventListener("click", (e) => {
   let studentId = takeInput(".sId").value;
   let className = takeInput(".className").value;
   let session = takeInput(".session").value;
-  let bangla = takeInput(".bangla").value;
-  let english = takeInput(".english").value;
-  let math = takeInput(".math").value;
-  let physics = takeInput(".physics").value;
-  let chemistry = takeInput(".chemistry").value;
-  let biology = takeInput(".biology").value;
+  let bangla = Number(takeInput(".bangla").value);
+  let english = Number(takeInput(".english").value);
+  let math = Number(takeInput(".math").value);
+  let physics = Number(takeInput(".physics").value);
+  let chemistry = Number(takeInput(".chemistry").value);
+  let biology = Number(takeInput(".biology").value);
   marks.push(bangla);
   marks.push(english);
   marks.push(math);
   marks.push(physics);
   marks.push(chemistry);
   marks.push(biology);
-  console.log(typeof bangla);
+
+  if (!firstName && !lastName && !studentId && !className) {
+    alert("Please fill up all input felds!");
+  } else {
+    studentForm.style.display = "none";
+    dashboardDetils.style.display = "block";
+    studentName.innerHTML = `Student Name: ${firstName} ${lastName}`;
+    idNumber.innerHTML = `Student Id: ${studentId}`;
+    studnetClass.innerHTML = `Class: ${className}`;
+    studentSession.innerHTML = `Session: ${session}`;
+    marks.map((mark) => {
+      totalScore += mark;
+      totalMark.innerHTML = `Total Mark: <span class="score">${totalScore}</span>/600`;
+    });
+
+    // Chart
+    new Chart(marksInfo, {
+      type: "bar",
+      data: {
+        labels: [
+          "Bangla",
+          "English",
+          "Mathematics",
+          "Physics",
+          "Chemistry",
+          "Biology",
+        ],
+        datasets: [
+          {
+            label: "Subject Marks",
+            data: marks,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+  clearInputs();
 });
